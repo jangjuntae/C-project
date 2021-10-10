cdedad36e5189b904b9fdc1b3916a08f9bbd78c5
juntae^-^ <91938434+jangjuntae@users.noreply.github.com>
2021-10-11 01:10:20 +0900

    Update C-project.c

--- C-project.c
+++ C-project.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include<windows.h>
+#include<conio.h>
+#include<time.h>
+#include<stdlib.h>
 
-int main(void){
-  dddd
+// 테트리스 보드의 크기
+#define Tetris_height 20;
+#define Tetris_width 10;
+
+// 키보드 값
+#define LEFT 75; // 좌로 이동
+#define RIGHT 77; // 우로 이동
+#define UP 72; // 회전
+#define DOWN 80; // 천천히 내려오기
+#define SPACE 32; // 빠르게 내려오기
+#define p 112 //일시정지 
+#define P 80 //일시정지
+#define ESC 27 //게임종료 
+
+//콘솔에서 그릴 x,y의 좌표
+
+
+int main(void) {
+
 }
